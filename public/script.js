2a102cc5edb2cb4697cb94320b2a78dd83a4194b
Kristiyan Lyubenov <KristiyanLyubenov26@gmail.com>
2024-05-01 18:06:08 +0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,60 @@
+
+const taskInput=document.getElementById("taskInput");
+const taskList=document.getElementById("taskList");
+const tasks = document.querySelectorAll("li");
+
+taskInput.textContent="nmo";
+
+  function addTask(){
+     if(taskInput.value==""){
+         window.alert("Enter a task First!");
+ 
+     }
+     else{
+        const newTask= document.createElement("li");
+        newTask.textContent=taskInput.value;
+ 
+        const span  = document.createElement("span");
+        span.innerHTML="\u00d7";
+        
+        newTask.appendChild(span);
+ 
+ 
+        taskList.appendChild(newTask);
+    
+       
+ 
+     }
+     taskInput.value="";
+     saveData();
+ 
+ };
+ 
+ 
+ 
+ taskList.addEventListener("click",event=>{
+   if(event.target.tagName=="LI"){
+    
+       event.target.classList.toggle("checked");
+       saveData();
+    
+     
+   }
+   else if(event.target.tagName=="SPAN"){
+     event.target.parentElement.remove();
+     saveData();
+   }
+ })
+ 
+ function saveData(){
+ 
+   localStorage.setItem("data",taskList.innerHTML);
+ }
+ 
+ function displayData(){
+   taskList.innerHTML=localStorage.getItem("data");
+ }
+ displayData();
+
+
+
---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+
+
+const port = 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+
+
+app.listen(port,()=>{
+    console.log(`Server listens to port: ${port}`);
+});
