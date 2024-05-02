d22bc2fbec2a299ed8cb8fba51974851820f9e02
Kristiyan Lyubenov <KristiyanLyubenov26@gmail.com>
2024-05-02 12:51:50 +0300

    removed unneceunnecessary code from static js

--- public/script.js
+++ public/script.js
@@ -1,35 +1,7 @@
 
-const taskInput=document.getElementById("taskInput");
 const taskList=document.getElementById("tasks");
-const tasks = document.querySelectorAll("li");
 
-//   function addTask(){
-//      if(taskInput.value==""){
-//          window.alert("Enter a task First!");
- 
-//      }
-//      else{
-//         const newTask= document.createElement("li");
-//         newTask.textContent=taskInput.value;
- 
-//         const span  = document.createElement("span");
-//         span.innerHTML="\u00d7";
-        
-//         newTask.appendChild(span);
- 
- 
-//         taskList.appendChild(newTask);
-    
-       
- 
-//      }
-//      taskInput.value="";
 
- 
-//  };
- 
- 
- 
  taskList.addEventListener("click",event=>{
    if(event.target.tagName=="LI"){
     
@@ -40,14 +12,6 @@ const tasks = document.querySelectorAll("li");
 
  })
  
-//  function saveData(){
- 
-//    localStorage.setItem("data",taskList.innerHTML);
-//  }
- 
-//  function displayData(){
-//    taskList.innerHTML=localStorage.getItem("data");
-//  }
- 
+
 
 
